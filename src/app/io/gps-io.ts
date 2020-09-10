c882e91ba325cbdc31d74f7543e83e716618734e
tentone <tentone@outlook.com>
2020-09-10 14:58:19 +0100

    Plugins

--- src/app/io/gps-io.ts
+++ src/app/io/gps-io.ts
@@ -1,6 +1,6 @@
-import {Modal} from '../screens/modal';
-import {Locale} from '../locale/locale';
 import {App} from '../app';
+import { Plugins } from '@capacitor/core';
+const {Geolocation} = Plugins;
 
 /**
  * Handles access to the GPS information, booth for mobile devices and the browser.
@@ -12,7 +12,7 @@ export class GpsIo {
      * @param onSuccess Method called when the position is obtained receives (longitude, latitude).
      * @param onError Method called if an error occurs while getting the GPS position.
      */
-    public static getPosition(onSuccess: Function, onError?: Function) {
+    public static async getPosition(onSuccess: Function, onError?: Function) {
         if (App.isMobile()) {
             // TODO <GET POSITION>
         } else if (navigator.geolocation) {
@@ -28,6 +28,9 @@ export class GpsIo {
      * @param onChange Method called when the position changes receives (longitude, latitude).
      */
     public static setWatcher(onChange: Function) {
+        Geolocation.watchPosition({}, (position, err) => {
+
+        });
         // TODO <WATCH GPS POSITION CONINOUSLY>
         /*// Watch for changes in the GPS position
         let watch = App.geolocation.watchPosition();
