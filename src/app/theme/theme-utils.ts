a94403ecf889c4d546da59040dd9dda1ab41dd80
tentone <tentone@outlook.com>
2021-12-04 22:35:15 +0000

    Theme

--- src/app/theme/theme-utils.ts
+++ src/app/theme/theme-utils.ts
@@ -1,17 +1,9 @@
+import {Theme} from './theme';
+
 /**
  * Handle integration with theming from the operative system.
  */
 export class ThemeOS {
-	/**
-	 * Light operative system theme.
-	 */
-	public static LIGHT: string = 'light';
-
-	/**
-	 * Dark operative system theme.
-	 */
-	public static DARK: string = 'dark';
-
 	/**
 	 * Get preferred theme based on the Operative System configurations.
 	 */
@@ -24,7 +16,7 @@ export class ThemeOS {
 	 */
 	public static watchChanges(handler: (theme: string)=> void): void {
 		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-			const theme: string = e.matches ? ThemeOS.DARK : ThemeOS.LIGHT;
+			const theme: string = e.matches ? Theme.DARK : Theme.LIGHT;
 			handler(theme);
 		});
 	}
