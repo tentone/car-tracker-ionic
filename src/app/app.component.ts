fc80be0377f9699ee9bbccfcb73335681b1f487b
tentone <tentone@outlook.com>
2021-12-04 00:40:11 +0000

    Splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import {TranslateService} from '@ngx-translate/core';
 import {Locale} from './locale/locale';
 import {App} from './app';
 import {Router} from '@angular/router';
-import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {AndroidPermissions} from '@ionic-native/android-permissions/ngx';
 import {Contacts} from '@ionic-native/contacts/ngx';
 import {SMS} from '@ionic-native/sms/ngx';
@@ -21,15 +20,8 @@ import {Geolocation} from '@ionic-native/geolocation/ngx';
   ]
 })
 export class AppComponent {
-  constructor(public platform: Platform, public splashScreen: SplashScreen, public translate: TranslateService, public router: Router,
-              public androidPermissions: AndroidPermissions, public sms: SMS, public contacts: Contacts, public file: File,
-              public chooser: Chooser, public geolocation: Geolocation) {
+  constructor(public platform: Platform, public translate: TranslateService, public router: Router, public androidPermissions: AndroidPermissions, public sms: SMS, public contacts: Contacts, public file: File, public chooser: Chooser, public geolocation: Geolocation) {
     Locale.initialize(this.translate);
     App.initialize(this.platform, this.router, androidPermissions, sms, contacts, file, chooser, geolocation);
-
-    this.platform.ready().then(() => {
-      this.splashScreen.hide();
-    });
   }
-
 }
