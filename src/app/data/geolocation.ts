c7ebe9141c978a1a90dcc17f99178042c794c76d
tentone <tentone@outlook.com>
2021-12-04 16:06:06 +0000

    Derp

--- src/app/data/geolocation.ts
+++ src/app/data/geolocation.ts
@@ -1,19 +1,29 @@
 /**
- * Represents a GPS position in the world map.
+ * Represents a position in the world map.
  */
 export class Geolocation {
     /**
-     * Latitude value of the position.
+     * Latitude in degrees relative to the meridian.
      */
     public latitude: number;
 
     /**
-     * Longitude value of the position.
+     * Longitude in degrees relative to the meridian.
      */
     public longitude: number;
 
-    public constructor(latitude?: number, longitude?: number) {
+    /**
+     * Altitude in meter relative to the sea level..
+     */
+    public altitude: number;
+
+    public accuracy: number = null;
+
+    public speed: number = null;
+
+    public constructor(latitude?: number, longitude?: number, altitude?: number) {
         this.latitude = latitude !== undefined ? latitude : 0;
         this.longitude = longitude !== undefined ? longitude : 0;
+        this.altitude = altitude !== undefined ? altitude : 0;
     }
 }
